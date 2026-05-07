44b3d6f11e9cbf89f585dd0f4e786d265a7b7690
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-05-07 17:31:54 +0200

    FEAT implemented methods of PizzaService

--- src/main/java/it/itsacademy/pizzeriaexpress/service/PizzaServiceImpl.java
+++ src/main/java/it/itsacademy/pizzeriaexpress/service/PizzaServiceImpl.java
@@ -1,7 +1,10 @@
 package it.itsacademy.pizzeriaexpress.service;
 
 import it.itsacademy.pizzeriaexpress.dto.PizzaDTO;
+import it.itsacademy.pizzeriaexpress.entity.Pizza;
+import it.itsacademy.pizzeriaexpress.exception.NotFoundException;
 import it.itsacademy.pizzeriaexpress.repository.PizzaRepository;
+import it.itsacademy.pizzeriaexpress.utility.mapper.PizzaMapper;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,23 +15,35 @@ public class PizzaServiceImpl implements PizzaService {
     @Autowired
     PizzaRepository repositoryPizza;
 
+    @Autowired
+    PizzaMapper mapper;
+
     @Override
     public PizzaDTO creaPizza(PizzaDTO nuovaPizza) {
-        return null;
+        Pizza saved = repositoryPizza.save(mapper.toEntity(nuovaPizza));
+        return mapper.toDTO(saved);
     }
 
     @Override
-    public PizzaDTO eliminaPizza(Integer idPizza) {
-        return null;
+    public PizzaDTO eliminaPizza(Long idPizza) {
+        PizzaDTO trovata = cercaPizza(idPizza);
+        repositoryPizza.deleteById(idPizza);
+        return trovata;
     }
 
     @Override
-    public PizzaDTO cercaPizza(Integer idPizza) {
-        return null;
+    public PizzaDTO cercaPizza(Long idPizza) {
+        Pizza trovata = repositoryPizza.findById(idPizza)
+                .orElseThrow(
+                        () -> new NotFoundException("Non è stato possibile trovare una pizza")
+                );
+        return mapper.toDTO(trovata);
     }
 
     @Override
-    public PizzaDTO modificaPizza(PizzaDTO nuovaPizza) {
-        return null;
+    public PizzaDTO modificaPizza(Long idPizza, PizzaDTO nuovaPizza) {
+        nuovaPizza.setIdPizza(idPizza);
+        Pizza saved = repositoryPizza.save(mapper.toEntity(nuovaPizza));
+        return mapper.toDTO(saved);
     }
 }
