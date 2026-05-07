16bc70b06488b167580498b350b8a38192c5b464
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-05-07 10:55:15 +0200

    Initialized

---
+++ src/main/java/it/itsacademy/pizzeriaexpress/PizzeriaExpressApplication.java
@@ -0,0 +1,13 @@
+package it.itsacademy.pizzeriaexpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzeriaExpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzeriaExpressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/itsacademy/pizzeriaexpress/PizzeriaExpressApplicationTests.java
@@ -0,0 +1,13 @@
+package it.itsacademy.pizzeriaexpress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzeriaExpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
