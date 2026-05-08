54f44e29d63faba4d964b0c2ab8c0bed6a8ddefa
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-05-08 10:16:09 +0200

    FEAT added dto for Rider

---
+++ src/main/java/it/itsacademy/pizzeriaexpress/dto/RiderDTO.java
@@ -0,0 +1,12 @@
+package it.itsacademy.pizzeriaexpress.dto;
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RiderDTO {
+    private Long idRider;
+
+    private String nome;
+}
