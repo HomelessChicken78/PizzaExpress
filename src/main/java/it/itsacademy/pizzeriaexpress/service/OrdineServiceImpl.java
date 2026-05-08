98327d32eb9fbc22fe92b8cc1ad16eb984e5f8f2
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-05-08 14:03:11 +0200

    FEAT: implemented Ordine business methods

--- src/main/java/it/itsacademy/pizzeriaexpress/service/OrdineServiceImpl.java
+++ src/main/java/it/itsacademy/pizzeriaexpress/service/OrdineServiceImpl.java
@@ -1,13 +1,18 @@
 package it.itsacademy.pizzeriaexpress.service;
 
 import it.itsacademy.pizzeriaexpress.dto.OrdineDTO;
+import it.itsacademy.pizzeriaexpress.dto.OrdinePizzaDTO;
+import it.itsacademy.pizzeriaexpress.dto.PizzaDTO;
+import it.itsacademy.pizzeriaexpress.entity.Ordine;
+import it.itsacademy.pizzeriaexpress.entity.Pizza;
+import it.itsacademy.pizzeriaexpress.exception.NotFoundException;
 import it.itsacademy.pizzeriaexpress.repository.OrdineRepository;
+import it.itsacademy.pizzeriaexpress.utility.mapper.OrdineMapper;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
-import java.util.List;
 
 @Service
 @Transactional
@@ -15,31 +20,51 @@ public class OrdineServiceImpl implements OrdineService {
     @Autowired
     OrdineRepository repositoryOrdine;
 
-    /*@Autowired
-    OrdineMapper mapper;*/
+    @Autowired
+    PizzaService pizzaService;
+
+    @Autowired
+    OrdineMapper mapper;
 
     @Override
     public OrdineDTO creaOrdine(OrdineDTO nuovoOrdine) {
-        return null;
+        Ordine saved = repositoryOrdine.save(mapper.toEntity(nuovoOrdine));
+
+        return mapper.toDTO(saved);
     }
 
     @Override
-    public OrdineDTO modificaOrdine(OrdineDTO ordineCambiato) {
+    public OrdineDTO modificaOrdine(String codiceOrdine, OrdineDTO ordineCambiato) {
+        ordineCambiato.setCodice(codiceOrdine);
+        cercaOrdine(codiceOrdine);
+        Ordine saved = repositoryOrdine.save(mapper.toEntity(ordineCambiato));
+
         return null;
     }
 
     @Override
     public OrdineDTO cercaOrdine(String codiceOrdine) {
-        return null;
+        Ordine trovato = repositoryOrdine.findById(codiceOrdine).orElseThrow(
+                () -> new NotFoundException("Non è stato possibile trovare un ordine con codice: " + codiceOrdine)
+        );
+
+        return mapper.toDTO(trovato);
     }
 
     @Override
     public Collection<OrdineDTO> tuttiGliOrdini() {
-        return List.of();
+        return mapper.toDTO(repositoryOrdine.findAll());
     }
 
     @Override
-    public OrdineDTO aggiungiPizza(String codiceOrdine, Long idPizza) {
-        return null;
+    public OrdineDTO aggiungiPizza(String codiceOrdine, Long idPizza, Integer quantita) {
+        PizzaDTO pizza = pizzaService.cercaPizza(idPizza);
+        OrdineDTO ordine = cercaOrdine(codiceOrdine);
+        ordine.getPizzeOrdinate().add(
+                new OrdinePizzaDTO(quantita, pizza)
+        );
+
+        Ordine saved = repositoryOrdine.save(mapper.toEntity(ordine));
+        return mapper.toDTO(saved);
     }
 }
