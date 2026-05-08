0426f570f94908fbfe9bf157764941425bd2bc77
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-05-08 10:01:25 +0200

    FEAT added method to search all the Pizzas

--- src/main/java/it/itsacademy/pizzeriaexpress/service/PizzaService.java
+++ src/main/java/it/itsacademy/pizzeriaexpress/service/PizzaService.java
@@ -1,6 +1,9 @@
 package it.itsacademy.pizzeriaexpress.service;
 
 import it.itsacademy.pizzeriaexpress.dto.PizzaDTO;
+import it.itsacademy.pizzeriaexpress.entity.Pizza;
+
+import java.util.Collection;
 
 public interface PizzaService {
     public PizzaDTO creaPizza(PizzaDTO nuovaPizza);
@@ -8,6 +11,8 @@ public interface PizzaService {
     public PizzaDTO eliminaPizza(Long idPizza);
 
     public PizzaDTO cercaPizza(Long idPizza);
+
+    public Collection<PizzaDTO> tutteLePizze();
 
     public PizzaDTO modificaPizza(Long idPizza, PizzaDTO nuovaPizza);
 }
--- src/main/java/it/itsacademy/pizzeriaexpress/service/PizzaServiceImpl.java
+++ src/main/java/it/itsacademy/pizzeriaexpress/service/PizzaServiceImpl.java
@@ -9,6 +9,8 @@ import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
+
 @Service
 @Transactional
 public class PizzaServiceImpl implements PizzaService {
@@ -38,6 +40,11 @@ public class PizzaServiceImpl implements PizzaService {
                         () -> new NotFoundException("Non è stato possibile trovare una pizza")
                 );
         return mapper.toDTO(trovata);
+    }
+
+    @Override
+    public Collection<PizzaDTO> tutteLePizze() {
+        return mapper.toDTO(repositoryPizza.findAll());
     }
 
     @Override
