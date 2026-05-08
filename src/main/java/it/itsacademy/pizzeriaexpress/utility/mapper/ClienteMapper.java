c81069e647461fc1bd44b5a2d2312ac8db372d73
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-05-08 14:43:49 +0200

    FEAT: added mapper for Cliente

---
+++ src/main/java/it/itsacademy/pizzeriaexpress/utility/mapper/ClienteMapper.java
@@ -0,0 +1,16 @@
+package it.itsacademy.pizzeriaexpress.utility.mapper;
+
+import it.itsacademy.pizzeriaexpress.dto.ClienteDTO;
+import it.itsacademy.pizzeriaexpress.entity.Cliente;
+import org.mapstruct.Mapper;
+
+import java.util.Collection;
+
+@Mapper
+public interface ClienteMapper {
+    Cliente toEntity(ClienteDTO dto);
+
+    ClienteDTO toDTO(Cliente entity);
+
+    Collection<ClienteDTO> toDTO(Collection<Cliente> entities);
+}
