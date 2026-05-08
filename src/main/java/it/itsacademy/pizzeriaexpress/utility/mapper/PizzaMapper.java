edaded2717a0342a6f3e70c2d8d5c1abf1513f0a
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-05-08 10:01:11 +0200

    FEAT added method to convert from a collection of entities to a collection of dtos

--- src/main/java/it/itsacademy/pizzeriaexpress/utility/mapper/PizzaMapper.java
+++ src/main/java/it/itsacademy/pizzeriaexpress/utility/mapper/PizzaMapper.java
@@ -4,9 +4,13 @@ import it.itsacademy.pizzeriaexpress.dto.PizzaDTO;
 import it.itsacademy.pizzeriaexpress.entity.Pizza;
 import org.mapstruct.Mapper;
 
+import java.util.Collection;
+
 @Mapper(componentModel = "spring")
 public interface PizzaMapper {
     public Pizza toEntity(PizzaDTO dto);
 
     public PizzaDTO toDTO(Pizza entity);
+
+    public Collection<PizzaDTO> toDTO(Collection<Pizza> entities);
 }
