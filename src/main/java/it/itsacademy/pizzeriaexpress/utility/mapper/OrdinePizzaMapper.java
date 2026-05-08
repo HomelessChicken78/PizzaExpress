477b973a5bc93ba7d01ebbc0fd662ab440321555
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-05-08 14:04:54 +0200

    REFACTOR: now OrdinePizzaMapper uses OrdineMapper

--- src/main/java/it/itsacademy/pizzeriaexpress/utility/mapper/OrdinePizzaMapper.java
+++ src/main/java/it/itsacademy/pizzeriaexpress/utility/mapper/OrdinePizzaMapper.java
@@ -5,7 +5,7 @@ import it.itsacademy.pizzeriaexpress.entity.OrdinePizza;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper
+@Mapper(uses = {PizzaMapper.class})
 public interface OrdinePizzaMapper {
     @Mapping(target = "id", ignore = true)
     OrdinePizza toEntity(OrdinePizzaDTO dto);
