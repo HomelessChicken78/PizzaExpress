fa6bdbba4f16bd6941f8def128c550e1b683548e
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-05-08 15:16:54 +0200

    FIX: OrdinePizzaMapper correctly is a Spring Bean now

--- src/main/java/it/itsacademy/pizzeriaexpress/utility/mapper/OrdinePizzaMapper.java
+++ src/main/java/it/itsacademy/pizzeriaexpress/utility/mapper/OrdinePizzaMapper.java
@@ -5,7 +5,7 @@ import it.itsacademy.pizzeriaexpress.entity.OrdinePizza;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper(uses = {PizzaMapper.class})
+@Mapper(componentModel = "spring", uses = {PizzaMapper.class})
 public interface OrdinePizzaMapper {
     @Mapping(target = "id", ignore = true)
     OrdinePizza toEntity(OrdinePizzaDTO dto);
