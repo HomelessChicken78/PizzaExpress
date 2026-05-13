91ac1ff5d9e056aabcb6a323fae31726985f3584
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-05-13 10:00:50 +0200

    FEAT: added endpoints for /api/pizze

--- src/main/java/it/itsacademy/pizzeriaexpress/controller/PizzaController.java
+++ src/main/java/it/itsacademy/pizzeriaexpress/controller/PizzaController.java
@@ -1,8 +1,44 @@
 package it.itsacademy.pizzeriaexpress.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import it.itsacademy.pizzeriaexpress.dto.PizzaDTO;
+import it.itsacademy.pizzeriaexpress.service.PizzaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
 
 @RestController
+@RequestMapping(path = "/pizze")
 public class PizzaController {
+    private static final String json = "application/json";
 
+    @Autowired
+    private PizzaService pizzaService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public PizzaDTO creaPizza(@RequestBody PizzaDTO dtoNuovaPizza) {
+        return pizzaService.creaPizza(dtoNuovaPizza);
+    }
+
+    @DeleteMapping(path = "/{idPizza}")
+    public PizzaDTO eliminaPizza(@PathVariable Long idPizza) {
+        return pizzaService.eliminaPizza(idPizza);
+    }
+
+    @GetMapping(path = "/{idPizza}")
+    public PizzaDTO cercaPizza(@PathVariable Long idPizza) {
+        return pizzaService.cercaPizza(idPizza);
+    }
+
+    @GetMapping
+    public Collection<PizzaDTO> cercaTutteLePizze() {
+        return pizzaService.tutteLePizze();
+    }
+
+    @PutMapping(path = "/{idPizza}")
+    public PizzaDTO modificaPizza(@PathVariable Long idPizza, @RequestBody PizzaDTO pizzaModificataDTO) {
+        return pizzaService.modificaPizza(idPizza, pizzaModificataDTO);
+    }
 }
