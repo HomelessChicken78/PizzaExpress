5bd5bce70715952d067b1425ccc721e3f8c920fe
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-05-13 12:08:40 +0200

    FEAT: Added missing dto

---
+++ src/main/java/it/itsacademy/pizzeriaexpress/dto/RegistraOrdineDTO.java
@@ -0,0 +1,18 @@
+package it.itsacademy.pizzeriaexpress.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Collection;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegistraOrdineDTO {
+    private String codice;
+
+    private Collection<AggiungiPizzaAllOrdineDTO> pizzeOrdinate;
+
+    private RiderDTO rider;
+}
