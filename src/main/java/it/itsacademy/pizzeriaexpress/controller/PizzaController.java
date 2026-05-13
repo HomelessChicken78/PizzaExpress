49eb859c37502b093b4b352cee1adc49a6d72562
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-05-13 10:04:18 +0200

    FEAT: added response and requested format type

--- src/main/java/it/itsacademy/pizzeriaexpress/controller/PizzaController.java
+++ src/main/java/it/itsacademy/pizzeriaexpress/controller/PizzaController.java
@@ -16,28 +16,28 @@ public class PizzaController {
     @Autowired
     private PizzaService pizzaService;
 
-    @PostMapping
+    @PostMapping(consumes = json, produces = json)
     @ResponseStatus(HttpStatus.CREATED)
     public PizzaDTO creaPizza(@RequestBody PizzaDTO dtoNuovaPizza) {
         return pizzaService.creaPizza(dtoNuovaPizza);
     }
 
-    @DeleteMapping(path = "/{idPizza}")
+    @DeleteMapping(path = "/{idPizza}", produces = json)
     public PizzaDTO eliminaPizza(@PathVariable Long idPizza) {
         return pizzaService.eliminaPizza(idPizza);
     }
 
-    @GetMapping(path = "/{idPizza}")
+    @GetMapping(path = "/{idPizza}", produces = json)
     public PizzaDTO cercaPizza(@PathVariable Long idPizza) {
         return pizzaService.cercaPizza(idPizza);
     }
 
-    @GetMapping
+    @GetMapping(produces = json)
     public Collection<PizzaDTO> cercaTutteLePizze() {
         return pizzaService.tutteLePizze();
     }
 
-    @PutMapping(path = "/{idPizza}")
+    @PutMapping(path = "/{idPizza}", produces = json, consumes = json)
     public PizzaDTO modificaPizza(@PathVariable Long idPizza, @RequestBody PizzaDTO pizzaModificataDTO) {
         return pizzaService.modificaPizza(idPizza, pizzaModificataDTO);
     }
