678db0ca48e78feede5f307247a75965b141bd81
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-05-14 15:41:36 +0200

    REFACTOR: Removed redundant public modified in interface

--- src/main/java/it/itsacademy/pizzeriaexpress/service/OrdineService.java
+++ src/main/java/it/itsacademy/pizzeriaexpress/service/OrdineService.java
@@ -6,13 +6,13 @@ import it.itsacademy.pizzeriaexpress.dto.RegistraOrdineDTO;
 import java.util.Collection;
 
 public interface OrdineService {
-    public OrdineDTO creaOrdine(Long idCliente, RegistraOrdineDTO nuovoOrdine);
+    OrdineDTO creaOrdine(Long idCliente, RegistraOrdineDTO nuovoOrdine);
 
-    public OrdineDTO cercaOrdine(Long idCliente, String codiceOrdine);
+    OrdineDTO cercaOrdine(Long idCliente, String codiceOrdine);
 
-    public Collection<OrdineDTO> tuttiGliOrdini();
+    Collection<OrdineDTO> tuttiGliOrdini();
 
-    public OrdineDTO aggiungiPizza(Long idCliente, String codiceOrdine, Long idPizza, Integer quantita);
+    OrdineDTO aggiungiPizza(Long idCliente, String codiceOrdine, Long idPizza, Integer quantita);
 
     OrdineDTO cambiaRider(Long idCliente, String codiceOrdine, Long idRider);
 }
