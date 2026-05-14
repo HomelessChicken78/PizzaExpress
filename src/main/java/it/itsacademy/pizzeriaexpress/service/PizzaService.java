34fd192c106a5cb826a77f5e70e66c342f8443fc
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-05-14 15:31:17 +0200

    REFACTOR: Removed redundant public modified in interface

--- src/main/java/it/itsacademy/pizzeriaexpress/service/PizzaService.java
+++ src/main/java/it/itsacademy/pizzeriaexpress/service/PizzaService.java
@@ -5,13 +5,13 @@ import it.itsacademy.pizzeriaexpress.dto.PizzaDTO;
 import java.util.Collection;
 
 public interface PizzaService {
-    public PizzaDTO creaPizza(PizzaDTO nuovaPizza);
+    PizzaDTO creaPizza(PizzaDTO nuovaPizza);
 
-    public PizzaDTO eliminaPizza(Long idPizza);
+    PizzaDTO eliminaPizza(Long idPizza);
 
-    public PizzaDTO cercaPizza(Long idPizza);
+    PizzaDTO cercaPizza(Long idPizza);
 
-    public Collection<PizzaDTO> tutteLePizze();
+    Collection<PizzaDTO> tutteLePizze();
 
-    public PizzaDTO modificaPizza(Long idPizza, PizzaDTO nuovaPizza);
+    PizzaDTO modificaPizza(Long idPizza, PizzaDTO nuovaPizza);
 }
