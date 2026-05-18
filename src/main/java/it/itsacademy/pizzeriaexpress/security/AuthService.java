345bde73edd43b0f6976e339024d217777b065d2
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-05-18 13:08:36 +0200

    STYLE: Removed unused import statement

--- src/main/java/it/itsacademy/pizzeriaexpress/security/AuthService.java
+++ src/main/java/it/itsacademy/pizzeriaexpress/security/AuthService.java
@@ -4,7 +4,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.stereotype.Service;
 
 /**
