053176277a16ac215b06f312ceb4c3d10e8eec4b
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-05-18 12:57:54 +0200

    CHORE: Disabled default behavior of spring security

--- src/main/java/it/itsacademy/pizzeriaexpress/PizzeriaExpressApplication.java
+++ src/main/java/it/itsacademy/pizzeriaexpress/PizzeriaExpressApplication.java
@@ -2,12 +2,33 @@ package it.itsacademy.pizzeriaexpress;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.security.autoconfigure.*;
 
-@SpringBootApplication
+/*
+ * Spring Security, se presente nel progetto, protegge automaticamente tutti gli endpoint HTTP dell'applicazione.
+ * Senza configurazioni personalizzate:
+ * - ogni richiesta richiede autenticazione;
+ * - viene creato automaticamente un utente di default:
+ *      username: user
+ *      password: generata casualmente e mostrata in console;
+ *
+ * SecurityAutoConfiguration:
+ * - abilita la configurazione automatica della sicurezza web
+ *   (login, filtri di sicurezza, protezione endpoint, ecc.).
+ *
+ * UserDetailsServiceAutoConfiguration:
+ * - crea automaticamente il servizio che gestisce l'utente di default
+ *   ("user") se non ne definiamo uno personalizzato.
+ *
+ * Escludendo entrambe le configurazioni:
+ * - l'applicazione NON richiede login;
+ * - tutti gli endpoint REST sono accessibili liberamente;
+ * - Spring Security non crea utenti automatici.
+ */
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class,
+		UserDetailsServiceAutoConfiguration.class})
 public class PizzeriaExpressApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PizzeriaExpressApplication.class, args);
 	}
-
 }
